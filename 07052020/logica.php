0a0de865f15422593c4d77990c3445d9c54a8535
Victor Manuel Sanchez Sanchez <vicool275@gmail.com>
2020-05-07 20:32:32 -0500

    Se agrego el ejercicio 07052020

--- 07052020/logica.php
+++ 07052020/logica.php
@@ -3,8 +3,7 @@ $numero1 = $_GET["numero1"];
 $numero2 = $_GET["numero2"];
 $numero3 = $_GET["numero3"];
 $resultado = $numero1 + $numero2 + $numero3;
-echo"Resultado es : ".$resultado;
+echo"<div align='center' style='background-color: #E74C3C; color: balck; border: 4px solid black;'>
+Resultado es : ".$resultado."
+</div>";
 ?>
-<div>
-
-</div>
