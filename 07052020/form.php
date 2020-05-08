4e921e6febd30f60cf85487e4b14fc5714def813
Victor Manuel Sanchez Sanchez <vicool275@gmail.com>
2020-05-07 19:13:38 -0500

    Se añadio el ejercicio 07052020

---
+++ 07052020/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suma de numeros PHP</title>
+</head>
+<body>
+    <form action="logica.php">
+        Numero 1: <input type="number" name="numero1"><br>
+        Numero 2: <input type="number" name="numero2"><br>
+        Numero 3: <input type="number" name="numero3"><br>
+        <button>Suma de numero</button>
+    </form>
+</body>
+</html>
---
+++ 07052020/logica.php
@@ -0,0 +1,10 @@
+<?php
+$numero1 = $_GET["numero1"];
+$numero2 = $_GET["numero2"];
+$numero3 = $_GET["numero3"];
+$resultado = $numero1 + $numero2 + $numero3;
+echo"Resultado es : ".$resultado;
+?>
+<div>
+
+</div>
